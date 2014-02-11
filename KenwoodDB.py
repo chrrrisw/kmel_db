c7e8e7a80e52e1a3af3af527625e8ff7030f6739
Chris Willoughby <chrrrisw@gmail.com>
2014-02-11 17:59:56 +1030

    start of performers, albums, playlists

--- KenwoodDB.py
+++ KenwoodDB.py
@@ -27,34 +27,70 @@ import struct
     genre_index_offset,
     genre_name_offset,
     genre_title_offset,
+    u5,
+    performer_index_offset,
+    performer_name_offset,
+    performer_title_offset,
+    u6,
+    album_index_offset,
+    album_name_offset,
+    album_title_offset,
+    u7,
+    u8,
+    playlist_index_offset,
+    playlist_name_offset,
+    playlist_title_offset,
+    u9,
+    u10,
+    u11,
+    u12,
+    u13,
     end
-) = list(range(25))
+) = list(range(43))
 
 file_offsets = {
-    signature:            (0x00, "<4s", "signature"),
-    u1:                   (0x04, "<HH", "u1"),
-    title_count:          (0x08, "<H", "title_count"),
-    title_entry_size:     (0x0a, "<H", "title_entry_size"),
-    genre_count:          (0x0c, "<H", "genre_count"),
-    genre_entry_size:     (0x0e, "<H", "genre_entry_size"),
-    performer_count:      (0x10, "<H", "performer_count"),
-    performer_entry_size: (0x12, "<H", "performer_entry_size"),
-    album_count:          (0x14, "<H", "album_count"),
-    album_entry_size:     (0x16, "<H", "album_entry_size"),
-    playlist_count:       (0x18, "<H", "playlist_count"),
-    playlist_entry_size:  (0x1a, "<H", "playlist_entry_size"),
-    u2:                   (0x1c, "<H", "u2"),
-    u3:                   (0x1e, "<H", "u3"),
-    main_index_offset:    (0x40, "<I", "main_index_offset"),
-    title_offset:         (0x44, "<I", "title_offset"),
-    shortdir_offset:      (0x48, "<I", "shortdir_offset"),
-    shortfile_offset:     (0x4c, "<I", "shortfile_offset"),
-    longdir_offset:       (0x50, "<I", "longdir_offset"),
-    longfile_offset:      (0x54, "<I", "longfile_offset"),
-    u4:                   (0x58, "<I", "u4"),
-    genre_index_offset:   (0x5c, "<I", "genre_index_offset"),
-    genre_name_offset:    (0x60, "<I", "genre_name_offset"),
-    genre_title_offset:   (0x64, "<I", "genre_title_offset")
+    signature:              (0x00, "<4s", "signature"),
+    u1:                     (0x04, "<HH", "u1"),
+    title_count:            (0x08, "<H", "title_count"),
+    title_entry_size:       (0x0a, "<H", "title_entry_size"),
+    genre_count:            (0x0c, "<H", "genre_count"),
+    genre_entry_size:       (0x0e, "<H", "genre_entry_size"),
+    performer_count:        (0x10, "<H", "performer_count"),
+    performer_entry_size:   (0x12, "<H", "performer_entry_size"),
+    album_count:            (0x14, "<H", "album_count"),
+    album_entry_size:       (0x16, "<H", "album_entry_size"),
+    playlist_count:         (0x18, "<H", "playlist_count"),
+    playlist_entry_size:    (0x1a, "<H", "playlist_entry_size"),
+    u2:                     (0x1c, "<H", "u2"),
+    u3:                     (0x1e, "<H", "u3"),
+    main_index_offset:      (0x40, "<I", "main_index_offset"),
+    title_offset:           (0x44, "<I", "title_offset"),
+    shortdir_offset:        (0x48, "<I", "shortdir_offset"),
+    shortfile_offset:       (0x4c, "<I", "shortfile_offset"),
+    longdir_offset:         (0x50, "<I", "longdir_offset"),
+    longfile_offset:        (0x54, "<I", "longfile_offset"),
+    u4:                     (0x58, "<I", "u4"),
+    genre_index_offset:     (0x5c, "<I", "genre_index_offset"),
+    genre_name_offset:      (0x60, "<I", "genre_name_offset"),
+    genre_title_offset:     (0x64, "<I", "genre_title_offset"),
+    u5:                     (0x68, "<I", "u5"),
+    performer_index_offset: (0x6c, "<I", "performer_index_offset"),
+    performer_name_offset:  (0x70, "<I", "performer_name_offset"),
+    performer_title_offset: (0x74, "<I", "performer_title_offset"),
+    u6:                     (0x78, "<I", "u6"),
+    album_index_offset:     (0x7c, "<I", "album_index_offset"),
+    album_name_offset:      (0x80, "<I", "album_name_offset"),
+    album_title_offset:     (0x84, "<I", "album_title_offset"),
+    u7:                     (0x88, "<I", "u7"),
+    u8:                     (0x8c, "<I", "u8"),
+    playlist_index_offset:  (0x90, "<I", "playlist_index_offset"),
+    playlist_name_offset:   (0x94, "<I", "playlist_name_offset"),
+    playlist_title_offset:  (0x98, "<I", "playlist_title_offset"),
+    u9:                     (0x9c, "<I", "u9"),
+    u10:                    (0xa0, "<I", "u10"),
+    u11:                    (0xa4, "<I", "u11"),
+    u12:                    (0xa8, "<I", "u12"),
+    u13:                    (0xac, "<I", "u13")
 }
 
 INDEX_FORMAT = "<HHHHIIIHHIHHIHHIHHIHHII"
@@ -179,6 +215,18 @@ class GenreIndexEntry(object):
         contents = "Genre: {}, titles: {}".format(self.name, str(self.titles))
         return contents
 
+class PerformerIndexEntry(object):
+    def __init__(self, values):
+        pass
+
+class AlbumIndexEntry(object):
+    def __init__(self, values):
+        pass
+
+class PlaylistIndexEntry(object):
+    def __init__(self, values):
+        pass
+
 class DBfile(object):
     def __init__(self, filename):
         f = open(filename, 'rb')
@@ -191,6 +239,9 @@ class DBfile(object):
 
         self.parse_main_index()
         self.parse_genres()
+        self.parse_performers()
+        self.parse_albums()
+        self.parse_playlists()
 
     def parse_main_index(self):
         self.entries = []
@@ -253,7 +304,42 @@ class DBfile(object):
 
             self.genres.append(genre_index_entry)
             current += self.details[genre_entry_size][0]
-        pass
+
+    def parse_performers(self):
+        self.performers = []
+        if self.details[performer_entry_size][0] != struct.calcsize(PERFORMER_INDEX_FORMAT):
+            print ("Unexpected performer index size")
+        current = self.details[performer_index_offset][0]
+        for index in range(self.details[performer_count][0]):
+            value = struct.unpack_from(PERFORMER_INDEX_FORMAT, self.db, current)
+            performer_index_entry = PerformerIndexEntry(value)
+
+            self.performers.append(performer_index_entry)
+            current += self.details[performer_entry_size][0]
+
+    def parse_albums(self):
+        self.albums = []
+        if self.details[album_entry_size][0] != struct.calcsize(ALBUM_INDEX_FORMAT):
+            print ("Unexpected album index size")
+        current = self.details[album_index_offset][0]
+        for index in range(self.details[album_count][0]):
+            value = struct.unpack_from(ALBUM_INDEX_FORMAT, self.db, current)
+            album_index_entry = AlbumIndexEntry(value)
+
+            self.albums.append(album_index_entry)
+            current += self.details[album_entry_size][0]
+
+    def parse_playlists(self):
+        self.playlists = []
+        if self.details[playlist_entry_size][0] != struct.calcsize(PLAYLIST_INDEX_FORMAT):
+            print ("Unexpected playlist index size")
+        current = self.details[playlist_index_offset][0]
+        for index in range(self.details[playlist_count][0]):
+            value = struct.unpack_from(PLAYLIST_INDEX_FORMAT, self.db, current)
+            playlist_index_entry = PlaylistIndexEntry(value)
+
+            self.playlists.append(playlist_index_entry)
+            current += self.details[playlist_entry_size][0]
 
     def show_titles(self):
         for index in range(self.details[title_count][0]):
