7e63e514c041ccc12ff5caa01fb4f6684727788b
Chris Willoughby <chrrrisw@gmail.com>
2015-10-30 14:43:01 +1030

    Store references to albums, add new methods.

--- src/PerformerIndexEntry.py
+++ src/PerformerIndexEntry.py
@@ -2,6 +2,10 @@ from BaseIndexEntry import BaseIndexEntry
 
 
 class PerformerIndexEntry(BaseIndexEntry):
+    '''A class to hold index data for performers.
+
+    Performers have titles and albums.
+    '''
 
     def __init__(self, name, titles, number):
         super(PerformerIndexEntry, self).__init__(name, titles, number)
@@ -12,6 +16,7 @@ class PerformerIndexEntry(BaseIndexEntry):
 
         # To be set later
         self._albums = []
+        self._album_numbers = []
         self._albums_initialised = False
 
         self._freeze()
@@ -21,22 +26,39 @@ class PerformerIndexEntry(BaseIndexEntry):
             self.__class__.__name__,
             self._number,
             self._name,
-            self.number_of_albums, self._albums,
+            self.number_of_albums, self._album_numbers,
             self.number_of_titles)
 
-    def init_albums(self):
+    def init_albums(self, albums):
         for title in self._titles:
-            if title.album_number not in self._albums:
-                self._albums.append(title.album_number)
+            if title.album_number not in self._album_numbers:
+                self._album_numbers.append(title.album_number)
+                self._albums.append(albums[title.album_number])
         self._albums_initialised = True
 
     @property
-    def albums(self):
+    def album_numbers(self):
         if self._albums_initialised:
-            return self._albums
+            return self._album_numbers
         else:
             raise Exception("Albums not initialised.")
 
     @property
     def number_of_albums(self):
-        return len(self._albums)
+        if self._albums_initialised:
+            return len(self._album_numbers)
+        else:
+            raise Exception("Albums not initialised.")
+
+    def album(self, album_number):
+        for a in self._albums:
+            if a.number == album_number:
+                return a
+        return None
+
+    def number_of_titles_for_album(self, album_number):
+        count = set()
+        for title in self._titles:
+            if title.album_number == album_number:
+                count.add(title.index)
+        return len(count)
