baa088e1e6cc503b9f0bcfbacf62327a6527550b
Chris Willoughby <chrrrisw@gmail.com>
2016-10-27 11:47:45 +1030

    Use psutil.disk_partitions to get FAT formatted partitions.

--- kmeldb/mounts.py
+++ kmeldb/mounts.py
@@ -1,13 +1,31 @@
 import os
+try:
+    import psutil
+except ImportError:
+    print('Falling back to parsing mounts output')
+    HAVE_PSUTIL = False
+else:
+    print('Using psutil')
+    HAVE_PSUTIL = True
 
 
 def get_fat_mounts():
+    # global HAVE_PSUTIL
+    # HAVE_PSUTIL = False
+
     fat_mounts = []
-    mounts = os.popen('mount')
-    for line in mounts.readlines():
-        device, ign1, mount_point, ign2, filesystem, options = line.split()
-        if 'fat' in filesystem:
-            fat_mounts.append((mount_point, filesystem, device))
+
+    if HAVE_PSUTIL:
+        partitions = psutil.disk_partitions()
+        for part in partitions:
+            if 'fat' in part.fstype:
+                fat_mounts.append((part.mountpoint, part.fstype, part.device))
+    else:
+        mounts = os.popen('mount')
+        for line in mounts.readlines():
+            device, ign1, mount_point, ign2, filesystem, options = line.split()
+            if 'fat' in filesystem:
+                fat_mounts.append((mount_point, filesystem, device))
     return fat_mounts
 
 
